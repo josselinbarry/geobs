deb04574b8a1cd20f048fdf293ff3d195c88205d
josselinbarry <josselin.barry@ofb.gouv.fr>
2023-04-18 11:12:44 +0200

    maj

--- src/imc.R
+++
@@ -1,9 +0,0 @@
-# Supprimer toutes les variables existantes
-rm(list = ls())
-# Création de mes variables taille et poids ; attention au point décimal
-poids <- 91
-taille <- 1.87
-# Calcul de l'IMC : poids sur taille au carré
-imc <- poids / (taille ^ 2)
-# Affichage du résultat
-print (imc)
--- src/initialisation_git_r.R
+++
@@ -1,9 +0,0 @@
-# install.packages()
-
-usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
-
-usethis::use_git_config(user.name = "josselinbarry", user.email = "josselin.barry@ofb.gouv.fr")
-
-usethis::use_git()
-
-usethis::use_github()
